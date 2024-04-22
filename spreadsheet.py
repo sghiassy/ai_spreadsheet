5c7aa2b126bc0f43a4651c69de39f9009da1ea9e
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-04-22 17:04:04 +0200

    Works

--- spreadsheet.py
+++ spreadsheet.py
@@ -8,6 +8,8 @@ from googleapiclient.errors import HttpError
 from openai import OpenAI
 from dotenv import load_dotenv
 import json
+import requests
+from vison_scraper import visionCrawl2
 
 load_dotenv()
 
@@ -43,6 +45,22 @@ def increment_cell(service):
         valueInputOption="USER_ENTERED", body=body).execute()
 
 
+def update_cell(service, new_value):
+    body = {"values": [[new_value]]}
+    result = (
+        service.spreadsheets()
+        .values()
+        .update(
+            spreadsheetId=SPREADSHEET_ID,
+            range=RANGE,
+            valueInputOption="USER_ENTERED",
+            body=body,
+        )
+        .execute()
+    )
+    print(f"result: {result}")
+
+
 def get_note(service):
     request = service.spreadsheets().get(
         spreadsheetId=SPREADSHEET_ID, ranges=RANGE, includeGridData=True
@@ -57,6 +75,31 @@ def get_note(service):
     else:
         return ''
         # print('No comments in cell D7')
+
+
+def update_note(service, new_note, prev_note):
+    request = service.spreadsheets().batchUpdate(
+        spreadsheetId=SPREADSHEET_ID,
+        body={
+            "requests": [
+                {
+                    "updateCells": {
+                        "range": {
+                            "sheetId": 0,  # Assuming the sheet ID is 0
+                            "startRowIndex": 6,  # 0-based, so 6 is row 7
+                            "endRowIndex": 7,
+                            "startColumnIndex": 3,  # 0-based, so 3 is column D
+                            "endColumnIndex": 4,
+                        },
+                        "rows": [{"values": [{"note": prev_note + "\n" + new_note}]}],
+                        "fields": "note",
+                    }
+                }
+            ]
+        },
+    )
+    response = request.execute()
+    return response
 
 
 def decipher_message(note):
@@ -88,9 +131,56 @@ def decipher_message(note):
 
     message = response.choices[0].message
     message_text = message.content
-    message_json = json.loads(message_text)
     print(f"message_text {message_text}")
+    message_json = json.loads(message_text)
     return message_json["url"], message_json['prompt']
+
+
+def fetch_webpage(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception if the request was unsuccessful
+    return response.text
+
+
+def scrape_web(url, user_prompt):
+    system_prompt = """
+    You are an expert web scraper. You will get the contents from a webpage and a question from the user.
+
+    Your goal is to answer the user's question with a number given the contents of the webpage.
+
+    You will be given information in the form of json, with the user's question in the field `question_from_user` and the relevant website information in the field `website_text`
+
+    Respond only with JSON. The JSON should be in the format:
+
+    {"value":<insert the answer to the user's question. Value should work with python's `int()` function>, "comment":<insert any comments you have per the user's question as text here"}
+
+    Ensure that the JSON is valid json and will work with python's `json.loads` method
+"""
+
+    webpage = fetch_webpage(url)
+
+    prompt = {"question_from_user": user_prompt, "website_text": webpage}
+
+    response = model.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        max_tokens=1024,
+    )
+
+    message = response.choices[0].message
+    message_text = message.content
+    print(f"message_text {message_text}")
+    message_json = json.loads(message_text)
+    return message_json["value"], message_json["comment"]
 
 
 def main():
@@ -119,9 +209,12 @@ def main():
         increment_cell(service)
         note = get_note(service)
         url, prompt = decipher_message(note)
-        # value, response = scrape_web(url, prompt)
+        value, ai_comment = visionCrawl2(url, prompt)
+        update_cell(service, value)
+        update_note(service, ai_comment, note)
 
-
+        # value, comment = scrape_web(url, prompt)
+        # print(f"value{value} comment{comment}")
 
     except HttpError as err:
         print(err)
--- vison_scraper.py
+++ vison_scraper.py
@@ -3,6 +3,7 @@ import subprocess
 import base64
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -46,7 +47,8 @@ def visionExtract(b64_image, prompt):
                 "role": "system",
                 "content": "You a web scraper, your job is to extract information based on a screenshot of a website & user's instruction",
             }
-        ] + [
+        ]
+        + [
             {
                 "role": "user",
                 "content": [
@@ -57,8 +59,8 @@ def visionExtract(b64_image, prompt):
                     {
                         "type": "text",
                         "text": prompt,
-                    }
-                ]
+                    },
+                ],
             }
         ],
         max_tokens=1024,
@@ -69,10 +71,62 @@ def visionExtract(b64_image, prompt):
 
     if "ANSWER_NOT_FOUND" in message_text:
         print("ERROR2: Answer not found")
-        return "I was unable to find the answer on that website. Please pick another one"
+        return (
+            "I was unable to find the answer on that website. Please pick another one"
+        )
     else:
         print(f"GPT: {message_text}")
         return message_text
+
+
+def visionExtract2(b64_image, prompt):
+    system_prompt = """
+    You a web scraper, your job is to extract information based on a screenshot of a website & user's instruction
+
+    Respond only with JSON. The JSON should be in the format:
+
+    {"value":<insert the answer to the user's question. Value should work with python's `int()` function>, "comment":<insert any comments you have per the user's question as text here>"}
+
+    Ensure that the JSON is valid json and will work with python's `json.loads` method
+"""
+    response = model.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt,
+            }
+        ]
+        + [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{b64_image}",
+                    },
+                    {
+                        "type": "text",
+                        "text": prompt,
+                    },
+                ],
+            }
+        ],
+        max_tokens=1024,
+    )
+
+    message = response.choices[0].message
+    message_text = message.content
+    print(f"message_text {message_text}")
+
+    if "ANSWER_NOT_FOUND" in message_text:
+        print("ERROR2: Answer not found")
+        return (
+            "I was unable to find the answer on that website. Please pick another one"
+        )
+
+    message_json = json.loads(message_text)
+    return message_json["value"], message_json["comment"]
 
 
 def visionCrawl(url, prompt):
@@ -84,6 +138,17 @@ def visionCrawl(url, prompt):
         return "I was unable to crawl that site. Please pick a different one."
     else:
         return visionExtract(b64_image, prompt)
+
+
+def visionCrawl2(url, prompt):
+    b64_image = url2screenshot(url)
+
+    print("Image captured")
+
+    if b64_image == "Failed to scrape the website":
+        return "I was unable to crawl that site. Please pick a different one."
+    else:
+        return visionExtract2(b64_image, prompt)
 
 
 if __name__ == "__main__":
